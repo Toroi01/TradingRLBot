c858b79cf139d119279bfb477749bc689a2b2a3b
elena-bonan <elena.bonan@king.com>
2021-05-23 00:19:46 +0200

    technical indicators

--- preprocessing/technical_indicators.py
+++ preprocessing/technical_indicators.py
@@ -9,16 +9,21 @@ indicators_list = [
     ('bbm', ta.volatility.bollinger_mavg, ['close']),
     ('atr', ta.volatility.average_true_range, ['high', 'low', 'close']),
     ('bbw', ta.volatility.bollinger_wband, ["close"]),
-    ( 'bbp', ta.volatility.bollinger_pband, ['close']),
+    ('bbp', ta.volatility.bollinger_pband, ['close']),
     ('bbhi',  ta.volatility.bollinger_hband, ['close']),
     ('bbli',  ta.volatility.bollinger_lband, ['close']),
     ('kcp', ta.volatility.keltner_channel_hband, ['high', 'low', 'close']) ,
     ('kchi', ta.volatility.keltner_channel_hband_indicator ,['high', 'low','close']),
-    ('kcli',ta.volatility.keltner_channel_hband, ['high', 'low', 'close'])
+    ('kcli',ta.volatility.keltner_channel_hband, ['high', 'low', 'close']),
+    ('macd', ta.trend.macd, ['close']),
+    ('macd_diff', ta.trend.macd_diff, ['close']),
+    ('mass_index', ta.trend.mass_index, ['high', 'low']),
+    ('dpo', ta.trend.dpo, ['close']),
+    ('kst_diff', ta.trend.aroon_up, ['close'])
 ]
 
-# Adding the indicators using the package ta. The default indicators are the one selected after have analyed the correlation.
-def add_indicators(df, indicators = ['atr', 'bbm', 'bbw', 'bbp', 'bbhi', 'bbli','kcp', 'kchi','kcli']):
+# Adding the indicators using the package ta. The default indicators are the one selected after have analyed the correlation.g
+def add_indicators(df, indicators = ['atr', 'bbm', 'bbw', 'bbp', 'bbhi', 'bbli','kcp', 'kchi','kcli', 'macd', 'macd_diff', 'mass_indes', 'dpo', 'kst_diff']):
     indicators_selected = [indicator for indicator in indicators_list if indicator[0] in indicators]
     df_temp = df.copy()
     for name, f, arg_names in indicators_selected:
@@ -43,6 +48,7 @@ def add_technical_indicators(df, indicators = ['macd', 'rsi_30', 'cci_30', 'dx_3
         for i in indicators:
             df_temp[i] = df_stocks[i]
         df_with_indicators = df_with_indicators.append(df_temp)
+    df_with_indicators.fillna(method='bfill', inplace=True)
     return df_with_indicators 
 
     
