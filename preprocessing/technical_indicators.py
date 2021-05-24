e92c78e11bf8ce78ca2372b447d31155f32ac39c
elena-bonan <elena.bonan@king.com>
2021-05-24 16:14:13 +0200

    adding new metrics

--- preprocessing/technical_indicators.py
+++ preprocessing/technical_indicators.py
@@ -17,25 +17,33 @@ indicators_list = [
     ('kcli',ta.volatility.keltner_channel_hband, ['high', 'low', 'close']),
     ('macd', ta.trend.macd, ['close']),
     ('macd_diff', ta.trend.macd_diff, ['close']),
+    ('adx', ta.trend.adx, ['high','low','close']),
     ('mass_index', ta.trend.mass_index, ['high', 'low']),
     ('dpo', ta.trend.dpo, ['close']),
-    ('kst_diff', ta.trend.aroon_up, ['close'])
+    ('kst', ta.trend.kst, ['close']),
+    ('aroon_up', ta.trend.aroon_up, ['close']),
+    ('aroon_down', ta.trend.aroon_down, ['close']),
+    ('ppo', ta.momentum.ppo, ['close'])
 ]
 
 # Adding the indicators using the package ta. The default indicators are the ones selected after have analysed the correlation.
-def add_indicators(df, indicators = ['atr', 'bbm', 'bbw', 'bbp', 'bbhi', 'bbli','kcp', 'kchi','kcli', 'macd', 'macd_diff', 'mass_indes', 'dpo', 'kst_diff']):
-    indicators_selected = [indicator for indicator in indicators_list if indicator[0] in indicators]
-    df_temp = df.copy()
-    for name, f, arg_names in indicators_selected:
-        wrapper = lambda func, args: func(*args)
-        args = [df[arg_name] for arg_name in arg_names]
-        df_temp[name] = wrapper(f, args)
-    df_temp.fillna(method='bfill', inplace=True)
-    return df_temp
+def add_indicators(df, indicators = ['atr', 'bbm', 'bbw', 'bbp', 'bbhi', 'bbli','kcp', 'kchi','kcli', 'macd', 'macd_diff', 'mass_index', 'dpo', 'kst','aroon_up', 'aroon_down','ppo']):
+    """This function add the indicators using the package ta"""
+    df_with_indicators = pd.DataFrame()
+    for ticker in df.ticker.unique():
+        df_temp = df[df["ticker"] == ticker].copy()
+        indicators_selected = [indicator for indicator in indicators_list if indicator[0] in indicators]
+        for name, f, arg_names in indicators_selected:
+            wrapper = lambda func, args: func(*args)
+            args = [df[arg_name] for arg_name in arg_names]
+            df_temp[name] = wrapper(f, args)
+        df_temp.fillna(method='bfill', inplace=True)
+        df_with_indicators = df_with_indicators.append(df_temp)
+    return df_with_indicators
 
 
 # Option using stockstats and considerin the most standard indicators used for trading
-def add_technical_indicators(df, indicators = ['macd', 'rsi_30', 'cci_30', 'dx_30']):
+def add_standard_indicators(df, indicators = ['macd', 'rsi_30', 'cci_30', 'dx_30']):
     """
     This function return the dataframe with the finantial indicators specify. See https://pypi.org/project/stockstats/ for the documentation. 
     """
